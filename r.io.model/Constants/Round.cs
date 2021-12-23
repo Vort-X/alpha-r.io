537bf49058b532c10f7d2e1ad592beb33ae3db4b
Mihailo <mihailpro02@gmail.com>
2021-12-23 03:00:53 +0200

    Temporarily added constants

---
+++ r.io.model/Constants/Round.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r.io.model.Constants
+{
+    //TODO: Move to config file
+    static class Round
+    {
+        private static readonly int Second = 1000;
+
+        public static readonly long Duration = 100 * Second;
+        public static readonly long PauseBeforeNext = 10 * Second;
+    }
+}
