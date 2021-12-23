a1d0631cff5930f64c0d4f7bc8d6acb62e2e34d2
Vort-X <mihailpro02@gmail.com>
2021-12-23 21:14:28 +0200

    entity to node mappers

---
+++ r.io.server/Mappers/AreaPartMapper.cs
@@ -0,0 +1,24 @@
+﻿using r.io.model.GameEntities;
+using r.io.shared.UdpGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r.io.server.Mappers
+{
+    internal static class AreaPartMapper
+    {
+        public static AreaPartNode ToNode(this AreaPart entity)
+        {
+            var node = new AreaPartNode
+            {
+                XLeft = entity.xLeft,
+                YTop = entity.yTop,
+                KillableObjects = entity.killableObjects.Select(o => o.ToNode()).ToList(),
+            };
+            return node;
+        }
+    }
+}
---
+++ r.io.server/Mappers/CircleGameObjectMapper.cs
@@ -0,0 +1,19 @@
+﻿using r.io.model.GameEntities;
+using r.io.shared.UdpGraph;
+
+namespace r.io.server.Mappers
+{
+    internal static class CircleGameObjectMapper
+    {
+        public static CircleGameObjectNode ToNode(this CircleGameObject entity)
+        {
+            var node = new CircleGameObjectNode
+            {
+                X = entity.x,
+                Y = entity.y,
+                Radius = entity.radius,
+            };
+            return node;
+        }
+    }
+}
