4024fa25540a24d2cadb0846294934070a5edf38
Mihailo <mihailpro02@gmail.com>
2021-12-26 14:40:49 +0200

    top players response investigation

--- r.io.server/PackageProcessing/TopPlayersResponseCreator.cs
+++ r.io.server/PackageProcessing/TopPlayersResponseCreator.cs
@@ -1,17 +1,31 @@
-﻿using r.io.shared.PackageProcessing;
+﻿using r.io.model.Services.Abstract;
+using r.io.server.Mappers;
+using r.io.server.Services;
+using r.io.shared;
+using r.io.shared.PackageProcessing;
 using r.io.shared.Services;
-using System;
+using r.io.shared.UdpGraph;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace r.io.server.PackageProcessing
 {
     class TopPlayersResponseCreator : ResponseCreator
     {
+        private BroadcastService broadcastService;
+        private ConnectionService connectionService;
+        private GameLoopManager gameLoopManager;
+        private Serializer<UdpPackage> serializer;
+
         public override GameServiceCollection GameServices 
         { 
             set
             {
-
+                broadcastService = value.Get<BroadcastService>();
+                connectionService = value.Get<ConnectionService>();
+                gameLoopManager = value.Get<GameLoopManager>();
+                serializer = value.Get<Serializer<UdpPackage>>();
             }
         }
 
@@ -21,7 +35,18 @@ namespace r.io.server.PackageProcessing
 
         public override Task[] Broadcast()
         {
-            return Array.Empty<Task>();
+            return connectionService.Connected.Select(conn =>
+            {
+                var pack = new UdpPackage() { Type = Type };
+                var top = gameLoopManager.gameService.getTopPlayers(10);
+                pack.Node = new TopPlayersNode()
+                {
+                    Players = new(top.Select(p => KeyValuePair.Create(p.name, p.ToNode()))),
+                };
+                var data = serializer.Serialize(pack);
+                return broadcastService.Send(conn.EndPoint, data);
+            }
+            ).ToArray();
         }
     }
 }
